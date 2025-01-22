e6aae9cb544fa3ea5e4883fb4200857022a1ac16
Lucivando Sousa <lucivandosousa@gmail.com>
2025-01-22 17:24:13 -0300

    feat: create the notfound page layout

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import './NotFound.css';
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="not-found">
+      <h1>Página Não Encontrada</h1>
+      <p>Ops! A página que você está procurando não existe.</p>
+    </div>
+  );
 }
