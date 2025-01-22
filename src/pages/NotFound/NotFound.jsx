5703bca12014fe40d368bc22e020479827e4b749
Lucivando Sousa <lucivandosousa@gmail.com>
2025-01-22 18:52:38 -0300

    style: adjust page style not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
 import './NotFound.css';
+import coffeeNotFound from '../../assets/images/coffee-notFound.png';
 
 export default function NotFound() {
   return (
     <div className="not-found">
       <h1>Página Não Encontrada</h1>
       <p>Ops! A página que você está procurando não existe.</p>
+      <Link to="/">Voltar para a página inicial</Link>
+      <div>
+        <img src={coffeeNotFound} alt="Página não encontrada" />
+      </div>
     </div>
   );
 }
