dbae167d43b87e4dc5b2e6e1164d7da07635a636
Lucivando Sousa <lucivandosousa@gmail.com>
2025-01-22 12:28:48 -0300

    feat: create the about page layout

---
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import './ImageCarousel.css';
+
+
+import foto1 from '../../assets/images/casal3.jpg';
+import foto2 from '../../assets/images/cafe1.jpeg';
+import foto3 from '../../assets/images/cafe2.jpeg';
+
+export default function ImageCarousel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={foto1}
+          alt="Casal Nanda e Junior"
+        />
+        <Carousel.Caption>
+          <h3>Nanda e Junior</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={foto2}
+          alt="Nossas delícias"
+        />
+        <Carousel.Caption>
+          <h3>Nossas delícias</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={foto3}
+          alt="Nosso espaço"
+        />
+        <Carousel.Caption>
+          <h3>Nosso espaço</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import './About.css';
+import ImageCarousel from '../../components/ImageCarousel/ImageCarousel';
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <section id="about" className="about-section">
+      <div className="container">
+        <h2>Sobre Nós</h2>
+        <p>O Café do Amanhã foi fundado por Nanda e Junior, dois apaixonados por café e pela cidade de Fortaleza.
+          Localizado na Avenida Beira Mar, nosso café oferece um refúgio acolhedor onde você pode desfrutar de
+          sabores únicos e de uma vista deslumbrante.</p>
+        <ImageCarousel />
+      </div>
+    </section>
+  );
 }
