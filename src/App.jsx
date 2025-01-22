8cc78161ff1d10842d76c5ca6978e3abcd513d93
Lucivando Sousa <lucivandosousa@gmail.com>
2025-01-22 11:12:53 -0300

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "./components/Header/Header"
+import Home from "./pages/Home/Home"
+import Footer from "./components/Footer/Footer"
+import About from "./pages/About/About"
+import Menu from "./pages/Menu/Menu"
+import OrderPage from "./pages/OrderPage/OrderPage"
+import Stories from "./pages/Stories/Stories"
+import Contact from "./pages/Contact/Contact"
+import NotFound from "./pages/NotFound/NotFound"
+
+export default function App() {
   return (
-    <>
-      <h1>Café do Amanhã</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/pedidos" element={<OrderPage />} />
+        <Route path="/historias" element={<Stories />} />
+        <Route path="/contato" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
-export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2025 Café do Amanhã. Todos os direitos reservados.</p>
+        <p>738 Av. Beira Mar, Fortaleza, CE | (85) 1234-5678</p>
+      </div>
+    </footer>
+  );
+}
+
