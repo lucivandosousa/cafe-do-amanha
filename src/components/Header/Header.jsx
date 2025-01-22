6dd908f193537a48ccc838c043307c945ba89c9c
Lucivando Sousa <lucivandosousa@gmail.com>
2025-01-22 12:36:16 -0300

    style: change internal header space

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,22 @@
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import './Header.css';
 
 export default function Header() {
   return (
     <Navbar expand="lg" bg="blue" variant="dark" sticky="top">
-      <Container>
-        <Navbar.Brand as={Link} to="/">Café do Amanhã</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">Início</Nav.Link>
-            <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
-            <Nav.Link as={Link} to="/menu">Menu</Nav.Link>
-            <Nav.Link as={Link} to="/pedidos">Pedidos</Nav.Link>
-            <Nav.Link as={Link} to="/historias">Histórias</Nav.Link>
-            <Nav.Link as={Link} to="/contato">Contato</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand as={Link} to="/">Café do Amanhã</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link as={Link} to="/">Início</Nav.Link>
+          <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+          <Nav.Link as={Link} to="/menu">Menu</Nav.Link>
+          <Nav.Link as={Link} to="/pedidos">Pedidos</Nav.Link>
+          <Nav.Link as={Link} to="/historias">Histórias</Nav.Link>
+          <Nav.Link as={Link} to="/contato">Contato</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
