462079687bafd001c43df0d0baa2647039f954c1
Lucivando Sousa <lucivandosousa@gmail.com>
2025-01-23 09:51:24 -0300

    style: adjust default page style

--- src/pages/PageDefault/PageDefault.jsx
+++ src/pages/PageDefault/PageDefault.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import './PageDefault.css'
 
 export default function PageDefault() {
   return (
-    <div>
-      <Header  />
-      <Outlet />
+    <div className="page-container">
+      <Header />
+      <main className='main-content'>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   )
